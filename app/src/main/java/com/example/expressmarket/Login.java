7cb1b99cddd3c7fbc0b51adf9bb5d3bc9455183c
Alberto177 <alberther199@gmail.com>
2022-10-24 18:25:51 -0500

    Login Rgistro

--- app/src/main/java/com/example/expressmarket/Login.java
+++ app/src/main/java/com/example/expressmarket/Login.java
@@ -2,7 +2,9 @@ package com.example.expressmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,5 +24,18 @@ public class Login extends AppCompatActivity {
         forg= findViewById(R.id.forgotTv);
         login= findViewById(R.id.loginBtn);
         noAccount= findViewById(R.id.noAccountTv);
+
+        noAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, RegistroUsuario.class));
+            }
+        });
+        forg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, OlviPass.class ));
+            }
+        });
     }
 }
