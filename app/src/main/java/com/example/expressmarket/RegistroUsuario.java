1189f2d30692966c18ddbfba39249134eb048778
Alberto177 <alberther199@gmail.com>
2022-10-24 19:57:44 -0500

    Login Rgistro

--- app/src/main/java/com/example/expressmarket/RegistroUsuario.java
+++ app/src/main/java/com/example/expressmarket/RegistroUsuario.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -11,6 +13,8 @@ public class RegistroUsuario extends AppCompatActivity {
 
     private ImageButton back,gps;
     private ImageView perfil;
+    private EditText name, phone, estado,ciudad, direccion, correo, pass, cpass;
+    private Button registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +22,16 @@ public class RegistroUsuario extends AppCompatActivity {
 
         back= findViewById(R.id.back);
         gps= findViewById(R.id.gps);
-        perfil= findViewById(R.id.perfilIv)
+        perfil= findViewById(R.id.perfilIv);
+        name= findViewById(R.id.nameEt);
+        phone= findViewById(R.id.phoneEt);
+        estado= findViewById(R.id.EstadoEt);
+        ciudad= findViewById(R.id.ciudadEt);
+        direccion= findViewById(R.id.addresEt);
+        correo= findViewById(R.id.emailEt);
+        pass= findViewById(R.id.passwordEt);
+        cpass= findViewById(R.id.cpasswordEt);
+        registrar= findViewById(R.id.registroBtn);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +49,12 @@ public class RegistroUsuario extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //imagen
+            }
+        });
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //registro de usuario
             }
         });
     }
