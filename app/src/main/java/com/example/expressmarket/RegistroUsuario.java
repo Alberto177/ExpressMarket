41050b521750d56596f5f1775a989a0f62bb63e7
Alberto177 <alberther199@gmail.com>
2022-10-24 19:51:00 -0500

    Login Rgistro

--- app/src/main/java/com/example/expressmarket/RegistroUsuario.java
+++ app/src/main/java/com/example/expressmarket/RegistroUsuario.java
@@ -3,12 +3,40 @@ package com.example.expressmarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class RegistroUsuario extends AppCompatActivity {
 
+    private ImageButton back,gps;
+    private ImageView perfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        back= findViewById(R.id.back);
+        gps= findViewById(R.id.gps);
+        perfil= findViewById(R.id.perfilIv)
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //detecta la posicion
+            }
+        });
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //imagen
+            }
+        });
     }
 }
