fff8321cde426beeba0eeb1a77ebf2896afe0033
Alberto177 <alberther199@gmail.com>
2022-10-25 20:53:13 -0500

    Login Rgistro

--- app/src/main/java/com/example/expressmarket/MainActivity.java
+++ app/src/main/java/com/example/expressmarket/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.expressmarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,17 @@ import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +29,53 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth= FirebaseAuth.getInstance();
+
         //Iniciar el login despues de 2sec
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, Login.class));
-                finish();
+                FirebaseUser user= firebaseAuth.getCurrentUser();
+                if(user==null){
+                    //usuario no logeado
+                    startActivity(new Intent(MainActivity.this, Login.class));
+                    finish();
+                }else{
+                    //user loggeado
+                    checkUserType();
+
+                }
             }
         }, 2000);
+    }
+
+    private void checkUserType() {
+        //si es vendedor se va al menu vendedor
+        //si es usuario se va al menu de usuario
+
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot ds: snapshot.getChildren()){
+                            String tipo= ""+ds.child("tipo").getValue();
+                            if (tipo.equals("Vendedor")){
+                                //usuario es vendedor
+                                startActivity(new Intent(MainActivity.this, MainVendedor.class));
+                                finish();
+                            }else{
+                                 //usuario es usuario
+                                startActivity(new Intent(MainActivity.this, MainUsuario.class));
+                                finish();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/expressmarket/OlviPass.java
+++ app/src/main/java/com/example/expressmarket/OlviPass.java
@@ -1,18 +1,30 @@
 package com.example.expressmarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class OlviPass extends AppCompatActivity {
 
     private ImageButton back;
     private EditText email;
     private Button recuperar;
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +34,11 @@ public class OlviPass extends AppCompatActivity {
         email= findViewById(R.id.emailEt);
         recuperar=findViewById(R.id.recoverBtn);
 
+        firebaseAuth= FirebaseAuth.getInstance();
+        progressDialog= new ProgressDialog(this);
+        progressDialog.setTitle("Por favor espera");
+        progressDialog.setCanceledOnTouchOutside(false);
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,5 +46,40 @@ public class OlviPass extends AppCompatActivity {
             }
         });
 
+        recuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recuperarPass();
+            }
+        });
+
+    }
+
+    private String emailn;
+    private void recuperarPass() {
+        emailn= email.getText().toString().trim();
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailn).matches()){
+            Toast.makeText(this, "Email invalido", Toast.LENGTH_SHORT).show();
+        }
+        progressDialog.setMessage("Enviando instrucciones para recuperar");
+        progressDialog.show();
+
+        firebaseAuth.sendPasswordResetEmail(emailn)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //instrucciones enviadas
+                        progressDialog.dismiss();
+                        Toast.makeText(OlviPass.this, "Instrucciones enviadas a tu correo ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //instrucciones no enviadas
+                        progressDialog.dismiss();
+                        Toast.makeText(OlviPass.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
